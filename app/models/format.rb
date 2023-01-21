743517eaa18ca02b339b87f837ca8d677238e8c5
RadioRadion <valentin.lassartesse@gmail.com>
2023-01-21 16:40:12 +0100

    Complete relations between basics models

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,2 +1,6 @@
 class Format < ApplicationRecord
+    has_many :edition_formats
+    has_many :comments
+    has_many :editions, through: :edition_formats
+    has_many :cards, through: :comments
 end
