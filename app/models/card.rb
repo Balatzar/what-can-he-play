247b1c29d6961afd3181be484f58efd4aa688fdf
RadioRadion <valentin.lassartesse@gmail.com>
2023-01-27 10:12:44 +0100

    order of validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
     has_many :card_editions
-    has_many :comments
     has_many :editions, through: :card_editions
+    has_many :comments
     has_many :formats, through: :comments
 end
