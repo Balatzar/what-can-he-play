223f305a04a60a4638158bde57e62d1bc4fb4210
Valentin Lassartesse <52502698+RadioRadion@users.noreply.github.com>
2023-01-27 10:19:24 +0100

    2 créer une seed (#14)
    * Basic file seed.rb
    
    * Complete relations between basics models
    
    * seed cleaned
    
    * resolve conversation github
    
    * order of validations

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -1,2 +1,6 @@
 class Edition < ApplicationRecord
+    has_many :card_editions
+    has_many :cards, through: :card_editions
+    has_many :edition_formats
+    has_many :formats, through: :edition_formats
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Editions
+edition_1 = Edition.find_or_create_by(name: "The Brothers War")
+
+# Red Cards
+card_1 = Card.find_or_create_by(name: "Myrel, Shield of Argive", minimal_cost: 4, blue: false, white: true, green: false, black: false, red: false)
+card_2 = Card.find_or_create_by(name: "Brotherhood's End", minimal_cost: 3, blue: false, white: false, green: false, black: false, red: true)
+
+# Formats
+format_1 = Format.find_or_create_by(name: "Standard")
+
+# Relation Cards <=> Editions
+CardEdition.find_or_create_by(card_id: card_1, edition_id: edition_1)
+CardEdition.find_or_create_by(card_id: card_2, edition_id: edition_1)
+
+# Relation Formats <=> Editions
+EditionFormat.find_or_create_by(format_id: format_1, edition_id: edition_1)
+
+# Commentaires
+Comment.find_or_create_by(card_id: card_1, format_id: format_1, text: "Créature à gros impact")
+Comment.find_or_create_by(card_id: card_2, format_id: format_1, text: "Un sweaper pour trois mana, peut aussi détruire tous les artefacts")
