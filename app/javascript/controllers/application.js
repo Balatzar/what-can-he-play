ff50d620f09c4cf0435b918984e75c44d79ea2f9
RadioRadion <valentin.lassartesse@gmail.com>
2023-02-17 11:00:04 +0100

    install stimulus nested and add comment_form on cards

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import NestedForm from 'stimulus-rails-nested-form'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+application.register('nested-form', NestedForm)
 
 export { application }
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,6 @@ class Card < ApplicationRecord
     has_many :card_editions
     has_many :editions, through: :card_editions
     has_many :comments
+    accepts_nested_attributes_for :comments, reject_if: :all_blank, allow_destroy: true
     has_many :formats, through: :comments
 end
