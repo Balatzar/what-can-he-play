c5299f87661ff29aff0ca13f6bd2c5e3c25e881e
Valentin Lassartesse <52502698+RadioRadion@users.noreply.github.com>
2023-02-18 12:57:49 +0100

    debug gems installed, annotate models launched (#19)

--- test/models/edition_format_test.rb
+++ test/models/edition_format_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: edition_formats
+#
+#  id         :bigint           not null, primary key
+#  format_id  :bigint           not null
+#  edition_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class EditionFormatTest < ActiveSupport::TestCase
